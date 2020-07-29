3849b73fd38a1cab19bd0c04a8eba5338aedb50b
sana <starsm2020@gmail.com>
2020-07-29 11:13:28 -0700

    Initial Commit

---
+++ Final Project/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Final Project
+//
+//  Created by Sana Manesh on 7/29/20.
+//  Copyright © 2020 Sana Manesh. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Final Project/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Final Project
+//
+//  Created by Sana Manesh on 7/29/20.
+//  Copyright © 2020 Sana Manesh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
