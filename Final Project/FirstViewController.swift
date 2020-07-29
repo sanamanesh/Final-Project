0b9017cfbb49ac42743569c41a50790b78967c2d
chloeneal <chloabella@gmail.com>
2020-07-29 13:53:25 -0700

    Version 3: coded label, etc. +added buttons

--- Final Project/FirstViewController.swift
+++ Final Project/FirstViewController.swift
@@ -10,6 +10,45 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var booksOfMonth: UILabel!
+    
+    
+    @IBOutlet weak var bookOneImage: UIImageView!
+    
+    
+    @IBOutlet weak var bookTitleOne: UILabel!
+    
+    
+    @IBOutlet weak var bookAuthorOne: UILabel!
+    
+    
+    
+    @IBOutlet weak var bookDescriptionOne: UITextView!
+    
+    
+    
+    @IBOutlet weak var bookTwoImage: UIImageView!
+    
+    
+    @IBOutlet weak var bookTwoTitle: UILabel!
+    
+    
+    @IBOutlet weak var bookAuthorTwo: UILabel!
+    
+    
+    @IBOutlet weak var bookDescriptionTwo: UITextView!
+    
+    
+    @IBOutlet weak var bookThreeImage: UIImageView!
+    
+    
+    @IBOutlet weak var bookTitleThree: UILabel!
+    
+    @IBOutlet weak var bookAuthorThree: UILabel!
+    
+    
+    @IBOutlet weak var bookDescriptionThree: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
