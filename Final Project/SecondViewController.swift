621e6cd1ccc8b28aaa2c29d3b2b0d6b32c311387
sara tabibian <sara@tabibian.com>
2020-07-29 21:37:33 -0700

    version 5: worked on profile page

--- Final Project/SecondViewController.swift
+++ Final Project/SecondViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
         // Do any additional setup after loading the view.
     }
+   
+    @IBOutlet weak var imageProfileButton: UIButton!
+//    @IBAction func imageProfileButton(_ sender: Any) {
+//        imageProfileButton.setImage(UIImage(systemName: "clearImage"), for: .normal)
+//       }
+    @IBOutlet weak var circleImage: UIImageView!
+    
+    @IBAction func imageProfileButton(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    @IBOutlet weak var displayImage: UIImageView!
+    
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {   displayImage.image = selectedImage         }
+        imagePicker.dismiss(animated: true, completion: nil)     }
 
-
+    
+//    func image;layer;.borderWidth = 1
+//    image.layer.masksToBounds = false
+//    image.layer.borderColor = UIColor.black.cgColor
+//    image.layer.cornerRadius = image.frame.height/2
+//    image.clipsToBounds = true
+    
 }
+
+
+
 
